9ede0e2dcf1cb6a70c1c68143b4618daecc8ac29
anshu-uppal <anshu.uppal@hcuge.ch>
2024-01-29 09:30:47 +0100

    copied over this file from another directory, to search for professions in the datasets

---
+++ code/load_data_dicts.R
@@ -0,0 +1,29 @@
+pacman::p_load(
+  tidyverse,
+  data.table,
+  here,
+  purrr)
+
+# Save the usual working directory
+a <- getwd()
+
+# Data dictionaries
+## Set the the data dictionaries folder as the WD
+setwd("P:/ODS/DMCPRU/UEPDATA/Specchio-COVID19/99_data/Internal_sharing_files/sugar_mapping_dictionaries_from_Julien/")
+
+## Load in all of the csv files
+temp = list.files(pattern="\\.csv$")
+myfiles = lapply(temp, fread)
+
+# d1 <- fread("ql_dict_SC19_health_behaviour_05_2022_20230922.csv")
+varlist <- c("préciser", "profession")
+tlist = tibble("outcome" = NA, "tempid" = 1:length(myfiles)) # empty dataframe
+# fill the dataframe with list elements TRUE/FALSE for the string searched below (can use | separator for OR terms)
+for (i in 1:length(myfiles)) {
+  tlist$outcome[i] <- any(myfiles[[i]] %like% varlist)
+}
+# Print the names of the files that have the searched string
+tlist <- tlist %>% filter(outcome);fhfh <- tlist$tempid;temp[fhfh]
+
+## Reset to original working directory
+# setwd(a)
