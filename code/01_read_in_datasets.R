4b60213f0af5da42e6daf3bfa39a698bab82a17d
anshu-uppal <anshu.uppal@hcuge.ch>
2024-01-31 14:07:01 +0100

    updated sources for some datasets

--- code/01_read_in_datasets.R
+++ code/01_read_in_datasets.R
@@ -6,7 +6,7 @@ pacman::p_load(
 # Read in datasets ####
 
 ## Inclusion ####
-### Parents ####
+### All ####
 inclusion <- readRDS("P:/ODS/DMCPRU/UEPDATA/Specchio-COVID19/99_data/Bases_for_sharing/2023-10-11-1616_ALLincsVs012345_ALLparticipants.rds") |> 
   filter(Origin != "V5_inclusion") |>
   filter(!testeur) |>  # remove data produced by testers - I think Nick already did this
@@ -16,8 +16,8 @@ inclusion <- readRDS("P:/ODS/DMCPRU/UEPDATA/Specchio-COVID19/99_data/Bases_for_s
     age = time_length(date_soumission - birthdate, "years"))
 
 ### KIDS inclusion ####
-incl_kids <- readRDS("P:/ODS/DMCPRU/UEPDATA/SEROCoV-KIDS/99_data/6_database/00_cleaned_augmented_database/2024-01-24_KIDS_inclusion_nov21_202401231106.rds") 
-incl_kids <- incl_kids |> 
+inc_kids <- readRDS("P:/ODS/DMCPRU/UEPDATA/SEROCoV-KIDS/99_data/6_database/00_cleaned_augmented_database/2024-01-24_KIDS_inclusion_nov21_202401231106.rds") 
+inc_kids <- inc_kids |> 
   mutate(
     date_soumission = as_date(date_soumission),
     age = time_length(date_soumission - birthdate, "years"))
@@ -33,8 +33,15 @@ incl_parents <- readRDS("P:/ODS/DMCPRU/UEPDATA/SEROCoV-KIDS/99_data/6_database/0
 birthdates <- inclusion |> select(codbar, birthdate)
 
 ## General Health ####
+### 2021 ####
+gh_21 <- readRDS("P:/ODS/DMCPRU/UEPDATA/Specchio-COVID19/99_data/Bases_for_sharing/2023-09-21-1217_SC19_general_health_31.05.2021-202309201254_ALL_cleaned_sym.rds") |> 
+  left_join(birthdates) |> 
+  mutate(
+    date_soumission = as_date(date_soumission),
+    age = time_length(date_soumission - birthdate, "years"))
+
 ### 2022 ####
-gh_22 <- readRDS("P:/ODS/DMCPRU/UEPDATA/Specchio-COVID19/99_data/Bases_for_sharing/2023-02-27-1537_Export_general_health-202206301210_ALL.rds")|> 
+gh_22 <- readRDS("P:/ODS/DMCPRU/UEPDATA/Specchio-COVID19/99_data/Bases_for_sharing/2023-09-21-1208_SC19_general_health_v1_02_2022-202309201254_ALL.rds") |> 
   left_join(birthdates) |> 
   mutate(
     date_soumission = as_date(date_soumission),
@@ -55,8 +62,9 @@ sommeil_23 <- readRDS("P:/ODS/DMCPRU/UEPDATA/Specchio-COVID19/99_data/Bases_for_
     date_soumission = as_date(date_soumission),
     age = time_length(date_soumission - birthdate, "years"))
 
-## Health Behavior: 2022 ####
-hb_22 <- readRDS("P:/ODS/DMCPRU/UEPDATA/Specchio-COVID19/99_data/Bases_for_sharing/2022-08-29-1248_export_comportement_santé-202206301206_ALL.rds")|> 
+## Health Behavior ####
+### 2022 ####
+hb_22 <- readRDS("P:/ODS/DMCPRU/UEPDATA/Specchio-COVID19/99_data/Bases_for_sharing/2023-09-21-1126_SC19_health_behaviour_05_2022-202309201254_ALL.rds")|> 
   left_join(birthdates) |> 
   mutate(
     date_soumission = as_date(date_soumission),
@@ -68,11 +76,23 @@ st_22 <- readRDS("P:/ODS/DMCPRU/UEPDATA/Specchio-COVID19/99_data/Bases_for_shari
   left_join(birthdates) |> 
   mutate(
     date_soumission = as_date(date_soumission),
-    age = time_length(date_soumission - birthdate, "years"))
+    age = time_length(date_soumission - birthdate, "years"),
+    profession = case_when(profession == "Techni^cien informatique" ~ "Technicien informatique",.default = profession)
+    )
 
-### 2023 (TBD) ####
+### 2023 ####
 st_23 <- readRDS("P:/ODS/DMCPRU/UEPDATA/Pour_user/Pour_Anshu/santé_travail_11_2023-202401221247.rds")|> # preliminary version from Sergeui
   left_join(birthdates) |>
   mutate(
     date_soumission = as_date(date_soumission),
     age = time_length(date_soumission - birthdate, "years"))
+
+# ## Monthly questionnaires ####
+# monthly <- readRDS("P:/ODS/DMCPRU/UEPDATA/Specchio-COVID19/99_data/Bases_for_sharing/2021-05-19-1212_readable_dat_2021.05.19_monthly_V3.rds") |> 
+#   mutate(date_soumission = as_date(date_sub)) |> 
+#   arrange(date_soumission)
+# am <- monthly |> group_by(codbar) |> filter(n()>1) |> 
+#   arrange(codbar, date_sub) |> 
+#   relocate(date_sub, .after = codbar_foyer)
+# 
+# length(unique(monthly$codbar))
