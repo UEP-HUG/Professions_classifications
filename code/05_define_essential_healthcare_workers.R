cc1cd53ed6a8af0a6b231f8b6016edef574f7c3d
anshu-uppal <anshu.uppal@hcuge.ch>
2024-03-06 11:56:13 +0100

    moved summary code to another file "Summaries.R", added variables on job sector to the final file from sant-travail 2022 and 2023

--- code/05_define_essential_healthcare_workers.R
+++ code/05_define_essential_healthcare_workers.R
@@ -13,11 +13,39 @@ if (file.exists(here("output", "cleaned_fuzzy_classified_occupations.rds"))) {
   source(here("code", "04b_long_clean_fuzzy_classifications.R"))
 }
 
+## Inclusion ####
 inclusion <- readRDS("P:/ODS/DMCPRU/UEPDATA/Specchio-COVID19/99_data/Bases_for_sharing/2023-10-11-1616_ALLincsVs012345_ALLparticipants.rds") |>
   filter(Origin != "V5_inclusion") |>
-  select(participant_id, codbar, serocov_work)
+  select(participant_id, codbar, serocov_work, serocov_pop, sp2_novdec_2020, sp3_juin_2021, sp4_avril_2022, pop_pilote)
 
-# Key occupations indices from ILO paper (Berg et al., 2023)
+## Sante-travail 2022 ####
+st_22 <- readRDS("P:/ODS/DMCPRU/UEPDATA/Specchio-COVID19/99_data/Bases_for_sharing/2023-09-21-1145_SanteTravail_ALLparticipants.rds") |> select(participant_id, job_sector) |> 
+  rename(job_sector.st_22 = job_sector)
+
+## Sante-travail 2023 ####
+st_23 <- readRDS("P:/ODS/DMCPRU/UEPDATA/Pour_user/Pour_Anshu/santé_travail_11_2023-202401221247.rds")|> # preliminary version from Sergeui
+  group_by(participant_id) |> filter(n() < 2) |> ungroup()  # remove duplicate entries
+
+sector_st_23 <- st_23 |> select(participant_id, starts_with("job_sector")) |> 
+  select(-job_sector_other_text) |>
+  mutate(
+    across(
+      # job_sector_commerce:job_sector_99,
+      starts_with("job_sector"),
+      ~na_if(., FALSE)
+    ))
+wc <- droplevels(col(sector_st_23, as.factor=TRUE)[which(sector_st_23 == "TRUE")])
+sector_st_23[levels(wc)] <- Map(factor, sector_st_23[levels(wc)], labels = levels(wc))
+sector_st_23 <- sector_st_23 |> 
+  unite("job_sector", job_sector_commerce:job_sector_99, na.rm = TRUE, sep = " ; ") |> 
+  mutate(job_sector = str_replace_all(job_sector, "job_sector_",""),
+         job_sector = str_replace(job_sector, "99","Other")
+  )
+st_23 <- left_join(st_23, sector_st_23) |> 
+  select(participant_id, job_sector) |> rename(job_sector.st_23 = job_sector)
+rm(sector_st_23, wc)
+
+# Key occupations indices from ILO paper (Berg et al., 2023) ####
 key_occupations <- read_xlsx(here("data", "indices_key_HCW.xlsx"), sheet = "key_occupations_ILO_ISCO2")
 HCW <- read_xlsx(here("data", "indices_key_HCW.xlsx"), sheet = "HCW_WHO")
 
@@ -28,12 +56,20 @@ merged <- left_join(occup_final_cleaned, key_occupations, by = join_by("isco_2" 
   left_join(HCW, by = join_by("isco_full" == "ISCO")) |> 
   # Merge with inclusion for serocov_work variable
   left_join(inclusion) |> # comment out for bus santé
+  left_join(st_22) |> 
+  left_join(st_23) |> 
   mutate(
     key_occupation = case_when(is.na(isco_full) ~ NA,
                                key_occupation == "TRUE" ~ TRUE, .default = FALSE), # define essential / key worker
     # serocov_work = case_when(serocov_work ~ "Yes", .default = "No"), # recode as Yes / No
+    # define health workers
     health_workers = case_when(is.na(isco_full) ~ NA,
-                               HCW == "Yes" ~ TRUE, .default = FALSE) # define health workers
+                               HCW == "Yes" ~ TRUE, .default = FALSE),
+    job_sector = case_when(
+      source == "st_22" ~ job_sector.st_22,
+      source == "st_23" ~ job_sector.st_23,
+      .default = NA
+    )
   ) |> 
   # filter(isco_full != -999) |> # remove unclassified people
   select(-c(HCW, label, occupational_grouping, Name_fr)) |>
@@ -45,6 +81,7 @@ merged <- left_join(occup_final_cleaned, key_occupations, by = join_by("isco_2" 
 # Save dataset ####
 ## Local ####
 # RDS
+saveRDS(merged, file = here("output", "ISCO_fuzzy_recoded_occupations.rds"))
 saveRDS(merged, file = here("output", paste0(format(Sys.time(), "%Y-%m-%d-%H%M_"),
                                             "ISCO_fuzzy_recoded_occupations.rds")))
 
@@ -60,39 +97,3 @@ saveRDS(merged, file = here("output", paste0(format(Sys.time(), "%Y-%m-%d-%H%M_"
 
 # Visually check that health workers are properly classified 
 health_w_check <- merged |> filter(health_workers == TRUE)
-
-# Summaries ####
-# define standard myflextable function 
-myflextable = function(data, ...) {
-  set_flextable_defaults(na_str = "NA", theme_fun = theme_booktabs, font.size = 12, padding.bottom = 1, padding.top = 1)
-  x = flextable(data, ...)
-  x = colformat_int(x, big.mark = "")
-  x = colformat_double(x, big.mark = "", digits = 2, na_str = "NA")
-  return(x)
-}
-
-## Key workers ####
-by_overall_key <- merged |> 
-  count(key_occupation) |> 
-  mutate(percent = paste0(round(n/sum(n)*100,1),"%")) |> 
-  mutate(serocov_work = "Overall") |> relocate(serocov_work)
-
-by_work_key <- merged |> 
-  group_by(serocov_work) |>
-  count(key_occupation) |> 
-  mutate(percent = paste0(round(n/sum(n)*100,1),"%"))
-# Bind them and output a summary table
-rbind(by_work_key, by_overall_key) |> myflextable()
-
-## Health workers ####
-by_overall_health <- merged |> 
-  count(health_workers) |> 
-  mutate(percent = paste0(round(n/sum(n)*100,1),"%")) |> 
-  mutate(serocov_work = "Overall") |> relocate(serocov_work)
-
-by_work_health <- merged |> 
-  group_by(serocov_work) |>
-  count(health_workers) |> 
-  mutate(percent = paste0(round(n/sum(n)*100,1),"%"))
-# Bind them and output a summary table
-rbind(by_work_health, by_overall_health) |> myflextable()
