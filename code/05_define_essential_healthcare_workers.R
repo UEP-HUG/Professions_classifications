f6a19779a250a1eb3153f0916d4088b873a7ccf2
anshu-uppal <anshu.uppal@hcuge.ch>
2024-03-27 18:22:05 +0100

    fix path to st_23 file

--- code/05_define_essential_healthcare_workers.R
+++ code/05_define_essential_healthcare_workers.R
@@ -58,7 +58,7 @@ st_22 <- readRDS("P:/ODS/DMCPRU/UEPDATA/Specchio-COVID19/99_data/Bases_for_shari
   mutate(sector_self_reported.st_22 = TRUE)
 
 ## Sante-travail 2023 ####
-st_23 <- readRDS("P:/ODS/DMCPRU/UEPDATA/Specchio-COVID19/Pour_user/Pour_Anshu/santé_travail_11_2023-202401221247.rds")|> # preliminary version from Sergeui
+st_23 <- readRDS("P:/ODS/DMCPRU/UEPDATA/Pour_user/Pour_Anshu/santé_travail_11_2023-202401221247.rds")|> # preliminary version from Sergeui
   group_by(participant_id) |> filter(n() < 2) |> ungroup()  # remove duplicate entries
 
 ### Unite job status ####
